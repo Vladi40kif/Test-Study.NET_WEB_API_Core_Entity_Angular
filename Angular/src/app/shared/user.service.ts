6c90d7a04f7db81b005c66986579972c35975a53
Vladi40kif <tymoshchuk.if@gmail.com>
2020-02-24 13:19:44 +0200

    end reg page, constains some problem, nut work:)

--- Angular/src/app/shared/user.service.ts
+++ Angular/src/app/shared/user.service.ts
@@ -12,25 +12,25 @@ export class UserService {
     responceRegister: any;
 
     formModel = this.fb.group({
-        userName: ['', [Validators.required, Validators.minLength(4)]],
+        userName: ['', [Validators.minLength(4)]],
         fullName: [''],
         email: ['', Validators.email],
         password: this.fb.group({
-            password: ['',[Validators.required, Validators.minLength(4)]],
-            confirmPass: ['', Validators.required]
+            password: ['',[Validators.minLength(4)]],
+            confirmPass: ['']
         }, { validator: this.checkPasswords })
     });
 
     sendRegInfo(){
-        this.http.post("https://localhost:44373/api/Auth/register",  {
-            UserName: this.formModel.value.UserName,
-            Password: this.formModel.value.password.password,
-            Email: this.formModel.value.Email,
-            Role: "",
-            FullName: this.formModel.value.FullName
-        }).subscribe(x => {
-            this.responceRegister = x;
-        });
+        // this.http.post("https://localhost:44373/api/Auth/register",  {
+        //     UserName: this.formModel.value.UserName,
+        //     Password: this.formModel.value.password.password,
+        //     Email: this.formModel.value.Email,
+        //     Role: "",
+        //     FullName: this.formModel.value.FullName
+        // }).subscribe(x => {
+        //     this.responceRegister = x;
+        // });
     }
 
     checkPasswords(group: FormGroup) {
