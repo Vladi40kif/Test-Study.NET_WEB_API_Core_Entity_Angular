ccf7599037e7cbec1246e848091cdf13bc67a374
Vladi40kif <tymoshchuk.if@gmail.com>
2020-02-28 17:31:17 +0200

    little bootstrup change in profile page

--- Angular/src/app/users/registrution/registrution.component.ts
+++ Angular/src/app/users/registrution/registrution.component.ts
@@ -16,7 +16,6 @@ export class RegistrutionComponent{
 
     register() {
         this.service.sendRegInfo().subscribe(resp => {
-           console.log(resp);
             if(resp.succeeded){
                 this.tostar.success("Register OK");
                 this.service.registerModel.InfoForm.reset();
